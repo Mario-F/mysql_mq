5dbd97901f94a698248219d685612fdf712c2122
Mario Fritschen <mario.fritschen@surfmedia.de>
2018-01-15 16:07:55 +0100

    Set indent size to 4 spaces

--- test/version040/index.js
+++ test/version040/index.js
@@ -5,14 +5,13 @@ const chai      = require('chai')
 const should    = chai.should()
 
 describe('Complete test mysql-mq', () => {
-  const store = {
-    queue: null,
-  }
+    const store = {
+        queue: null,
+    }
 
-  describe('DUMMY', () => {
-    it('TEST', (done) => {
-      done()
+    describe('DUMMY', () => {
+        it('TEST', (done) => {
+            done()
+        })
     })
-  })
-
 })
