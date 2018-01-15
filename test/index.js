5ea7aa24179eb4925e3dc72e65ed106a1fe03a9e
Mario Fritschen <mario.fritschen@surfmedia.de>
2018-01-15 20:02:29 +0100

    Add wtfnode on tests to find blocking tasks

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 'use strict'
+const wtf = require('wtfnode')
 
 describe('Version 0.2.0 - TEST', () => {
   require('./version020')
@@ -6,4 +7,10 @@ describe('Version 0.2.0 - TEST', () => {
 
 describe('Version 0.4.0 - TEST', () => {
   require('./version040')
+})
+
+describe('END', () => {
+    it('Check running requests', (done) => {
+        wtf.dump()
+    })
 })
