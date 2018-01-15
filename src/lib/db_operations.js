0b0123e5759929734461a84de58cb21acf2f45b0
Mario Fritschen <mario.fritschen@surfmedia.de>
2018-01-15 20:02:49 +0100

    Reworked db_operations (WIP)

--- src/lib/db_operations.js
+++ src/lib/db_operations.js
@@ -2,13 +2,31 @@
 const debug     = require('debug')('mysqlmq:lib:dboperations')
 const mysql     = require('mysql2')
 
+/**
+ * Handles database connection and queries including creating table structure if not exists
+ * @constructor
+ * @param {object} - mysql related configuration options
+ * @param {string} queuename - Name of the queue
+ */
 module.exports = function dbOperations(config, queuename) {
+    // Object to return
     const expose = {}
-    const connection = mysql.createConnection(config)
 
+    // Handle queries executed before table structure create is finished
+    let ready = false
+    const qPending = []
+    function execWhenReady(execCall) {
+        if(ready) return execCall()
+        qPending.push(execCall)
+    }
+
+    // On Function call setup database structure
+    // TODO: This is an async call, following query calls must be delayed till this statement returns
+    debug('Create table structure for queue', queuename, 'if not exists')
+    const connection = mysql.createConnection(config)
     connection.query(require('../sql/db_statements').create_table_data, [queuename], (err, result) => {
-        if(err)
-            throw err
+        if(err) throw err
+        ready = true
     })
 
     expose.end = (callback) => {
@@ -18,14 +36,17 @@ module.exports = function dbOperations(config, queuename) {
     }
 
     expose.query = (query, params, callback) => {
-        connection.query(query, params, (err, res) => {
-            if(config.persistent) {
-                return connection.end((err) => {
-                    if(err) return callback(err)
-                })
-            }
-            if(err) return callback(err)
-            return callback(null, res)
+        execWhenReady(() => {
+            connection.query(query, params, (err, res) => {
+                if(!config.persistent) {
+                    return connection.end((err) => {
+                        if(err) return callback(err)
+                        return callback(null, res)
+                    })
+                }
+                if(err) return callback(err)
+                return callback(null, res)
+            })
         })
     }
 
