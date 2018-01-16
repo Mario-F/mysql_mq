68816dc9e6ce23484ecf75fd4bf9850eec07b051
Mario Fritschen <mario.fritschen@surfmedia.de>
2018-01-16 14:18:36 +0100

    Rewrite db_operations to be more robust

--- src/lib/db_operations.js
+++ src/lib/db_operations.js
@@ -12,42 +12,71 @@ module.exports = function dbOperations(config, queuename) {
     // Object to return
     const expose = {}
 
+    // Variables
+    let iCountQueries = 0
+
     // Handle queries executed before table structure create is finished
     let ready = false
     const qPending = []
     function execWhenReady(execCall) {
-        if(ready) return execCall()
+        if(ready)
+            return execCall()
+
+        debug('Query was set for pending')
         qPending.push(execCall)
     }
 
     // On Function call setup database structure
-    // TODO: This is an async call, following query calls must be delayed till this statement returns
     debug('Create table structure for queue', queuename, 'if not exists')
     const connection = mysql.createConnection(config)
     connection.query(require('../sql/db_statements').create_table_data, [queuename], (err, result) => {
-        if(err) throw err
-        ready = true
+        debug('Create table structure finished')
+        if(err)
+            throw err
+
+        connection.end((err) => {
+            debug('Create table connection ended')
+            if(err)
+                throw err
+
+            ready = true
+            debug('Executing pending queries')
+            qPending.forEach(cl => cl())
+        })
     })
 
+    // Create execution function and passes it to execWhenReady, will execute when dbOperations is initialized
+    expose.query = (query, params, callback) => {
+        // Add id to query for debugging
+        iCountQueries++
+        const idQuery = iCountQueries
+        debug(idQuery, 'Query calling')
+
+        // Pass query to executor
+        execWhenReady(() => {
+            debug(idQuery, 'Query executing')
+            const qConnection = mysql.createConnection(config)
+            qConnection.query(query, params, (qErr, res) => {
+                debug(idQuery, 'Query finished')
+                return qConnection.end((eErr) => {
+                    debug(idQuery, 'Query ended')
+                    if(qErr)
+                        return callback(qErr)
+                    if(eErr)
+                        return callback(eErr)
+
+                    return callback(null, res)
+                })
+            })
+        })
+    }
+
+    // Deprecated: Remove when concept for persistent and non persistent connections in final stage
     expose.end = (callback) => {
         if(!config.persistent)
             return callback(null)
-        connection.end(callback)
-    }
 
-    expose.query = (query, params, callback) => {
-        execWhenReady(() => {
-            connection.query(query, params, (err, res) => {
-                if(!config.persistent) {
-                    return connection.end((err) => {
-                        if(err) return callback(err)
-                        return callback(null, res)
-                    })
-                }
-                if(err) return callback(err)
-                return callback(null, res)
-            })
-        })
+        connection.end(callback)
     }
 
     return expose
