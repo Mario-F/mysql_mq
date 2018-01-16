d7c9874e047b64643f00a05d53bcbf8f1fcc62a6
Mario Fritschen <mario.fritschen@surfmedia.de>
2018-01-16 14:19:38 +0100

    Add wtfnode to tests. Activate by set DEBUG env.

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 'use strict'
-const wtf = require('wtfnode')
+// When debug is used check for blocking async tasks at end of tests
+if(process.env.DEBUG) {
+    var wtf = require('wtfnode') // eslint-disable-line no-inner-declarations
+}
 
 describe('Version 0.2.0 - TEST', () => {
   require('./version020')
@@ -9,8 +12,12 @@ describe('Version 0.4.0 - TEST', () => {
   require('./version040')
 })
 
-describe('END', () => {
-    it('Check running requests', (done) => {
-        wtf.dump()
+// When debug is used check for blocking async tasks at end of tests
+if(process.env.DEBUG) {
+    describe('WTF NDOE', () => {
+        it('Check running requests', (done) => {
+            wtf.dump()
+            done()
+        })
     })
-})
+}
